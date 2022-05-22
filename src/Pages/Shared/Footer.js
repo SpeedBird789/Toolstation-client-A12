a1e13c05293a6909225196191d9505d31674bb07
SpeedBird789 <asifmahmoud787@gmail.com>
2022-05-22 20:01:25 +0100

    navbar and banner structure done

---
+++ src/Pages/Shared/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-center font-bold'>All rights reserved © </p>
+        </div>
+    );
+};
+
+export default Footer;
