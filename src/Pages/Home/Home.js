6ceec32337e861ea8558075f9061ffc0d04f6d25
SpeedBird789 <asifmahmoud787@gmail.com>
2022-05-23 21:45:52 +0100

    tools data fetched from server

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import BusinessSummary from '../BusinessSummary/BusinessSummary';
 import Banner from './Banner';
+import Tools from './Tools';
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Tools></Tools>
+            <BusinessSummary></BusinessSummary>
         </div>
     );
 };
---
+++ src/Pages/Home/Tools.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Tool from './Tool';
+
+const Tools = () => {
+    const [tools, setTools] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/tool')
+        .then(res => res.json())
+        .then(data => setTools(data))
+    })
+    return (
+        <div className='my-28'>
+            <div className='text-center'>
+                <h2 className='text-primary  text-2xl font-bold uppercase'>Our Tools</h2>
+                <h3 className='text-3xl'>Get the best tools for your DIY or any other needs</h3>
+            </div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+                {
+                    tools.map(tool => <Tool key={tool._id} tool={tool}></Tool>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Tools;
