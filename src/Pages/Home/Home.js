985f4dc15711e03feae9325f85bb3c7b31557030
SpeedBird789 <asifmahmoud787@gmail.com>
2022-05-24 00:05:21 +0100

    reviews added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BusinessSummary from '../BusinessSummary/BusinessSummary';
 import Banner from './Banner';
 import Newsletter from './Newsletter';
+import Testimonials from './Testimonials';
 import Tools from './Tools';
 
 
@@ -11,6 +12,7 @@ const Home = () => {
             <Banner></Banner>
             <div className='px-16'>
             <Tools></Tools>
+            <Testimonials></Testimonials>
             <BusinessSummary></BusinessSummary>
             </div>
             <Newsletter></Newsletter>
---
+++ src/Pages/Home/Review.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Review = ({review}) => {
+
+    return (
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+        <div className="card-body">
+            <div className="flex items-center">
+                <div className="avatar">
+                    <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
+                        <img src={review.img} alt=""/>
+                    </div>
+                </div>
+                <div>
+                    <h4 className='text-xl'>{review.name}, {review.location}</h4>
+                    <p>{review.description}</p>
+                    <div>
+                    <div className="rating mt-2">
+                        <input type="radio" name="rating-1" class="mask mask-star" />
+                        <input type="radio" name="rating-1" class="mask mask-star" />
+                        <input type="radio" name="rating-1" class="mask mask-star" />
+                        <input type="radio" name="rating-1" class="mask mask-star" />
+                        <input type="radio" name="rating-1" class="mask mask-star" />                
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+};
+
+export default Review;
